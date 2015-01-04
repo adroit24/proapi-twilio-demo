24598e0c2a448b7bde8ebf0774dd913030621895
Kushal <adroit24@yahoo.com>
2015-01-04 11:47:01 -0800

    init

--- lib/application.rb
+++ lib/application.rb
@@ -26,6 +26,11 @@ module WP
       def proapi_result_message
         
         puts params[:Body]
+        phone_re = ^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$
+
+        is_match = phone_re.match(params[:Body])
+ 
+        puts is_match
 
         number = format_number(params[:From])
 
